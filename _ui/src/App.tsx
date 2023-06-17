e61fa82615c7144690553d73ffed73ffc45659a1
And1 <92379947+And1x@users.noreply.github.com>
2023-06-17 19:27:01 +0200

    initial commit

---
+++ _ui/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center">Go React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/pogo
+
+go 1.19
